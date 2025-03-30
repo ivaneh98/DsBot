89a92792ed452d34523dfdabad8e45aff38d5cde
ivaneh98 <50576867+ivaneh98@users.noreply.github.com>
2025-03-30 23:29:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://ivaneh:9OsRHexcIxoEpLWk@discordbot.quehgnq.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
